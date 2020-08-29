1e79f8cbad95c49710b25d47cb6bd1800eb25489
camboya <64973355+camboya@users.noreply.github.com>
2020-08-29 13:09:21 -0300

    Update ejemplos_clase.py

--- ejemplos_clase.py
+++ ejemplos_clase.py
@@ -12,7 +12,7 @@ Programa creado para mostrar ejemplos prácticos de los visto durante la clase
 
 __author__ = "Inove Coding School"
 __email__ = "alumnos@inove.com.ar"
-__version__ = "1.3"
+__version__ = "1.21"
 
 import sys
 import os
